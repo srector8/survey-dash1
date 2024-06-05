947fedf502812be58b2d7f7ce3bb352b81764178
srector8 <157165984+srector8@users.noreply.github.com>
2024-06-05 15:03:00 -0400

    Update survey_data_dashboard.py

--- survey_data_dashboard.py
+++ survey_data_dashboard.py
@@ -126,7 +126,7 @@ def plot_comparison_data(data, question, game_days):
             st.subheader(f'Game Day: {game_day}')
             game_day_data = data[(data['game_day'] == game_day) & (data['question'] == question)]
             percentages_table = (game_day_data['choice_text'].value_counts(normalize=True).sort_index() * 100).round(1)
-            percentages_table.index = percentages_table.index.astype(str) + "%"
+            percentages_table = percentages_table.apply(lambda x: f'{x:.1f}%')
             st.table(percentages_table)
 
 if __name__ == "__main__":
