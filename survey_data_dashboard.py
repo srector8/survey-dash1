b970f860f37f1c8c3f40c27becc346d3b1f4eb5c
srector8 <157165984+srector8@users.noreply.github.com>
2024-07-02 15:00:55 -0400

    Update survey_data_dashboard.py

--- survey_data_dashboard.py
+++ survey_data_dashboard.py
@@ -168,7 +168,6 @@ def plot_comparison_data(data, question, game_days):
             tooltip=['choice_text', alt.Tooltip('percentage:Q', format='.1f')],
         ).properties(
             title=f'Game Day: {game_day}',
-            width=bar_width  # Set the width of the chart
         )
     
         charts.append(chart)
