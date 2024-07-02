552a49f3ba348adbf5cc8db060479bb7017192bc
srector8 <157165984+srector8@users.noreply.github.com>
2024-07-02 15:22:45 -0400

    Update survey_data_dashboard.py

--- survey_data_dashboard.py
+++ survey_data_dashboard.py
@@ -205,7 +205,7 @@ def plot_average_ratings(data, selected_rating_questions):
 
         # Create a bar chart using Altair
         chart = alt.Chart(average_ratings).mark_bar().encode(
-            x=alt.X('game_day:O', title='Game Day'),
+            x=alt.X('game_day_label:O', title='Game Day'),
             y=alt.Y('choice_text:Q', title='Average Rating'),
             tooltip=['game_day_label', 'choice_text']
         ).transform_calculate(
